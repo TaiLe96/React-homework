77bd32755aa04f42dac3065dc48f7e19f1da5991
Tai Le <tle449@gmail.com>
2020-02-07 20:05:09 -0800

    import searchbar into navbar and insert in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar"
-
+import Navbar from "./components/Navbar";
 
 
 function App(){
     return(
         <div>
             <Header/>
-            <SearchBar/>
+            {/* <SearchBar/> */}
+            <Navbar/>
         </div>
     )
 }
