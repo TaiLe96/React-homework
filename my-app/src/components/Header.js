b7be38683a6c243a7617a466be60462b8ea32540
Tai Le <tle449@gmail.com>
2020-02-07 17:19:48 -0800

    initial commit

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Header(){
+    return(
+        <div>
+            <nav className="header">
+                <h1>Employee Directory</h1>
+                <h5>Hello Everybody</h5>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
